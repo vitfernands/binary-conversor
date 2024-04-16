54f95f19c3fa2a0c90a213bd47a3161ff7c6a6aa
brenolo04 <brenocaires04@gmail.com>
2024-04-16 10:35:59 -0300

    Variaveis mais legiveis e estrutura do script mais semantica

--- script.js
+++ script.js
@@ -1,84 +1,116 @@
-{{ 
 const select = {
-    inputSystem: document.getElementById("select-number-system"),
-    outputNumber: document.getElementById("select-number-system"),
+    input: document.getElementById("select-number-system"),
+    output: document.getElementById("select-result-number-system"),
 };
-
 const buttonCalculate = document.getElementById("buttonCalculate");
-let resultNumber = document.getElementById("resultNumber");
+    
+const inputNumber = document.getElementById("inputNumber");
+let resultNumber = document.getElementById("resultNumber"); // Elemento que ira imprimir o resultado
 
-let input;
-let output;
-let number;
+    // Variveis de controle que irão receber o select
+let inputSystemNumber = 'decimal';
+let outputSystemNumber = 'BinaryResult';
 
-select.numberSystem.addEventListener("change", () => input = select.inputSystem.value);
-select.outputNumber.addEventListener("change", () => output = select.outputNumber.value);  //eventos para checar o que esta selecionado no <select> do html
+select.input.addEventListener("change", () => inputSystemNumber = select.input.value);
+select.output.addEventListener("change", () => {outputSystemNumber = select.output.value
+    console.log(outputSystemNumber)
 
+});  //eventos para checar o que esta selecionado no <select> do html
 
-buttonCalculate.onclick = converge
+buttonCalculate.addEventListener("click", converge)
 
 function converge() {
-    let number = parseInt(inputNumber.value);                           //É NECESSARIO TROCAR O NUMBER PELO InputNumber para dar certo a conversao
+        let number = parseInt(inputNumber.value);                     //É NECESSARIO TROCAR O NUMBER PELO InputNumber para dar certo a conversao
 
-    if(input === 'decimal') {
-        switch(output) {
-            case 'BinaryResult': {
-                resultNumber.value = number.toString(2);
+        if(inputSystemNumber === 'decimal') {
+            switch(outputSystemNumber) {
+                case 'BinaryResult': {
+                    resultNumber.value = number.toString(2);
+                    break;
+                };
+                case 'OctalResult': {
+                    resultNumber.value = number.toString(8);
+                    break;
+                };
+                case 'HexaResult': {
+                    resultNumber.value = number.toString(16).toUpperCase();
+                    break;
+                };
+                default: {
+                    resultNumber.value = inputNumber.value
+                    break;
+                }
             };
-            case 'DecimalResult': {
-                resultNumber.value = number.toString(8);
+        };
+        if(inputSystemNumber === 'binary') {
+            switch(outputSystemNumber) {
+                case 'OctalResult': {
+                    resultNumber.value = number.toString(8);
+                    break;
+                };
+                case 'DecimalResult': {
+                    resultNumber.value = number.toString(10);
+                    break;
+                };
+                case 'HexaResult': {
+                    number = parseInt(inputNumber.value, 2)
+                    number = number.toString(16).toUpperCase()
+                    resultNumber.value = number
+                    break;
+                };
+                default: {
+                    resultNumber.value = inputNumber.value
+                    break;
+                }
             };
-        }
-    }
-};
-
-}}
-// buttonCalculate.onclick = function () {
-//     let number = parseInt(inputNumber.value);                           //É NECESSARIO TROCAR O NUMBER PELO InputNumber para dar certo a conversao
-//     let selectInputNumberOption = selectNumberSystem.value;
-//     let selectOutputNumberOption = selectResultNumberSystem.value;  
-
-//     if(selectInputNumberOption === "optionDecimal" && selectOutputNumberOption === "optionBinaryResult") {  //decimal para outros
-//         resultNumber.value = number.toString(2);
-//     } else if (selectInputNumberOption === "optionDecimal" && selectOutputNumberOption === "optionOctalResult") {  
-//         resultNumber.value = number.toString(8);
-//     } else if (selectInputNumberOption === "optionDecimal" && selectOutputNumberOption === "optionHexadecimalResult") {
-//         resultNumber.value = number.toString(16).toUpperCase();
-
-//     } else if (selectInputNumberOption === "optionBinary" && selectOutputNumberOption === "optionDecimalResult") {  //binario para outros
-//         resultNumber.value = parseInt(inputNumber.value, 2);
-//     } else if (selectInputNumberOption === "optionBinary" && selectOutputNumberOption === "optionOctalResult") {
-//         number = parseInt(inputNumber.value, 2)
-//         number = number.toString(8)
-//         resultNumber.value = number
-//     } else if (selectInputNumberOption === "optionBinary" && selectOutputNumberOption === "optionHexadecimalResult") {
-//         number = parseInt(inputNumber.value, 2)
-//         number = number.toString(16).toUpperCase()
-//         resultNumber.value = number
-
-//     } else if (selectInputNumberOption === "optionOctal" && selectOutputNumberOption === "optionDecimalResult") {   //octal para outros
-//         resultNumber.value = parseInt(inputNumber.value, 8)
-//     } else if (selectInputNumberOption === "optionOctal" && selectOutputNumberOption === "optionBinaryResult") {
-//         number = parseInt(inputNumber.value, 8)
-//         number = number.toString(2)
-//         resultNumber.value = number
-//     } else if (selectInputNumberOption === "optionOctal" && selectOutputNumberOption === "optionHexadecimalResult") {
-//         number = parseInt(inputNumber.value, 8)
-//         number = number.toString(16).toUpperCase()
-//         resultNumber.value = number
-
-//     } else if (selectInputNumberOption === "optionHexadecimal" && selectOutputNumberOption === "optionDecimalResult") {     //hexadecimal para outros
-//         number = parseInt(inputNumber.value, 16)
-//         resultNumber.value = number
-//     } else if (selectInputNumberOption === "optionHexadecimal" && selectOutputNumberOption === "optionBinaryResult") {
-//         number = parseInt(inputNumber.value, 16)
-//         number = number.toString(2)
-//         resultNumber.value = number
-//     } else if (selectInputNumberOption === "optionHexadecimal" && selectOutputNumberOption === "optionOctalResult") {
-//         number = parseInt(inputNumber.value, 16)
-//         number = number.toString(8)
-//         resultNumber.value = number
-//     }
-// }
-
-
+        };
+        if(inputSystemNumber === 'octal') {
+            switch(outputSystemNumber) {
+                case 'BinaryResult': {
+                    number = parseInt(inputNumber.value, 8)
+                    number = number.toString(2)
+                    resultNumber.value = number
+                    break;
+                };
+                case 'DecimalResult': {
+                    resultNumber.value = parseInt(inputNumber.value, 8)
+                    break;
+                };
+                case 'HexaResult': {
+                    number = parseInt(inputNumber.value, 8)
+                    number = number.toString(16).toUpperCase()
+                    resultNumber.value = number
+                    break;
+                };
+                default: {
+                    resultNumber.value = inputNumber.value
+                    break;
+                };
+            };
+        };
+        if(inputSystemNumber === 'hexa') {
+            switch(outputSystemNumber) {
+                case 'BinaryResult': {
+                    number = parseInt(inputNumber.value, 16);
+                    number = number.toString(2);
+                    resultNumber.value = number;
+                    break;
+                };
+                case 'OctalResult': {
+                    number = parseInt(inputNumber.value, 16);
+                    number = number.toString(8);
+                    resultNumber.value = number;
+                    break;
+                };
+                case 'DecimalResult': {
+                    number = parseInt(inputNumber.value, 16)
+                    resultNumber.value = number;
+                    break;
+                };
+                default: {
+                    resultNumber.value = inputNumber.value;
+                    break;
+                };
+            };
+        };
+    };
