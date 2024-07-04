61ec3190f442450dbc950dfb865c3e84a392161d
Vítor Prochnow <161534480+vitfernands@users.noreply.github.com>
2024-07-03 23:02:18 -0300

    coletando elementos
    Continuando o código aos poucos, coletei os elementos em HTML para o JavaScript.

---
+++ index.js
@@ -0,0 +1,5 @@
+let selecInput = document.getElementById("select-input-number-system").value;
+let selectOutput = document.getElementById("select-output-number-system").value;
+let inputNumber = document.getElementById("input-number").value;
+let button = document.getElementById("button-calculate");
+let resultNumber = document.getElementById("result-number").value;
