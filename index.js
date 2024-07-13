b1877856c282889d6271d536f587822acf16d015
Vítor Prochnow <161534480+vitfernands@users.noreply.github.com>
2024-07-13 16:12:10 -0300

    pegando o option selecionado

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let selecInput = document.getElementById("select-input-number-system").value;
-let selectOutput = document.getElementById("select-output-number-system").value;
+let selectInput = document.getElementById("select-input-number-system");
+let selectOutput = document.getElementById("select-output-number-system");
 let inputNumber = document.getElementById("input-number").value;
 let button = document.getElementById("button-calculate");
 let resultNumber = document.getElementById("result-number").value;
+
+function verifySelectedOption () {
+    const selectedInputOption = selectInput.options[selectInput.selectedIndex].value;
+    const selectedOutputOption = selectOutput.options[selectOutput.selectedIndex].value;
+    alert("input: " + selectedInputOption + " output: " + selectedOutputOption);
+}
+
+button.addEventListener("click", () => {
+    verifySelectedOption();
+});
