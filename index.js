4af83a87448c41840ad67e7f92423b1deb12e37c
Vítor Prochnow <161534480+vitfernands@users.noreply.github.com>
2024-07-28 18:54:50 -0300

    Criando funções, criando lógica
    Criei uma função para pegar o valor do inputNumber e estabeleci uma lógica no Notepad++

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let button = document.getElementById("button-calculate");
 let resultNumber = document.getElementById("result-number").value;
 //resultNumber = parseInt(resultNumber);
 
+function getInputnumber() {
+    let inputNumber = document.getElementById("input-number").value;
+    inputNumber = parseInt(inputNumber);
+    return inputNumber;
+}
+
 function verifyInputSelectedOption () {
     const selectedInputOption = selectInput.options[selectInput.selectedIndex].value;
     return selectedInputOption;
@@ -14,14 +20,20 @@ function verifyOutputSelectedoption() {
     return selectedOutputOption;
 }
 
+function calculate() {
+    
+}
+
 button.addEventListener("click", () => {
-    let inputNumber = document.getElementById("input-number").value;
-    inputNumber = parseInt(inputNumber); //fazer uma funcao para pegar esses valores
+    const inputNumber = getInputnumber();
 
     const inputOption = verifyInputSelectedOption();
+
     const outputOption = verifyOutputSelectedoption();
 
     console.log(inputNumber, typeof inputNumber);
+
+    calculate();
 });
 
 
